0c3694668bcabb8b90ca0f0922ce46bcb8178d33
delafqm@163.com <delafqm>
2020-05-28 16:47:22 +0800

    1

---
+++ Demo/AutoMapper/AutoMapperForWebApp2.2/AutoMapper/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMapperForWebApp2._2.AutoMapper
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+
+
+    public class UserDto
+    {
+        public string Name { get; set; }
+    }
+
+    public class User1
+    {
+        public string Name1 { get; set; }
+    }
+
+
+    public class UserDto1
+    {
+        public string Name1 { get; set; }
+    }
+}
